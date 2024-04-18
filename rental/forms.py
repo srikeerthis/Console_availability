be5dd439af3f1b773c29ec4030bef3870b91295a
srikeerthis <srikeerthisrinivasan@gmail.com>
2024-04-18 18:29:22 -0500

    BREAKING CHANGE:Updated attributes to view statuses

--- rental/forms.py
+++ rental/forms.py
@@ -4,5 +4,8 @@ from .models import Console
 class ConsoleForms(forms.ModelForm):
     class Meta:
         model = Console
-        fields = ['name','is_rented']
+        fields = ['name','status']
+        widgets = {
+            'status':forms.RadioSelect(choices=Console.STATUS_CHOICES)
+        }
 
