58c45edd155ce678c01f9f7a531cf1663dcab418
srikeerthis <srikeerthisrinivasan@gmail.com>
2024-04-18 18:30:24 -0500

    BREAKING CHANGE: Updated attributes to view statuses of consoles

--- rental/models.py
+++ rental/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Console(models.Model):
+    STATUS_CHOICES = [
+        ('available', 'Available'),
+        ('rented', 'Rented'),
+        ('repair', 'Under Repair'),
+    ]
+    
     name = models.CharField(max_length=100)
-    is_rented = models.BooleanField(default=False)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='available')
 
     def __str__(self):
         return self.name
